68f3cc7ade0acfd50b53e42d43479a224c08a955
†¤¤† †¤¤† <yaolsher@yandex.ru>
2022-08-28 18:23:55 +0300

    Рефаторинг и решения

---
+++ src/main/java/ParkingSystem.java
@@ -0,0 +1,25 @@
+public class ParkingSystem {
+    int[] carContainer = new int[3];
+
+    public ParkingSystem(int big, int medium, int small) {
+        carContainer[0] = big;
+        carContainer[1] = medium;
+        carContainer[2] = small;
+    }
+    public  void main(String[] args) {
+        ParkingSystem parkingSystem = new ParkingSystem(1, 1, 0);
+        System.out.println(parkingSystem.addCar(1));
+        System.out.println(parkingSystem.addCar(2));
+        System.out.println(parkingSystem.addCar(3));
+        System.out.println(parkingSystem.addCar(1));
+    }
+
+
+    public boolean addCar(int carType) {
+        if (carContainer[carType - 1] > 0) {
+            carContainer[carType - 1]--;
+            return true;
+        }
+        return false;
+    }
+}
